b6ab0a1cf4863e4e0db111240e0cc6939defbf98
Anuar Tapia <anuartap@ciencias.unam.mx>
2015-12-04 14:21:21 -0600

    Crea más datos dummy y trata de debuggear pero no sale

--- peticiones.php
+++ peticiones.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set("America/Mexico_City");
-$p = array(
+$p1 = array(
       "cliente" => "Jessi Jaime",
       "telefono" => 0123456789,
       "fecha" => date('ymd'),
@@ -16,6 +16,22 @@ $p = array(
       "cancelada" => false,
       "atendida" => false
       );
+$p2 = array(
+      "cliente" => "Zaide Aguilar",
+      "telefono" => 9876543210,
+      "fecha" => date('ymd'),
+      "imagenMapa" => "https://cbtelevision.com.mx/wp-content/uploads/2015/06/619.jpg",
+      "calleOrigen" => "Grimmauld Place",
+      "numeroOrigen" => "12",
+      "coloniaOrigen" => "London",
+      "delegacionOrigen" => "UK",
+      "calleDestino" => "16 de Septiembre",
+      "numeroDestino" => "15A",
+      "coloniaDestino" => "Tepepan",
+      "delegacionDestino" => "Xochimilco",
+      "cancelada" => false,
+      "atendida" => false
+      );
 /*
 $jsontext = "['peticiones':{";
 foreach($p as $key => $value) {
@@ -25,6 +41,8 @@ $jsontext = substr_replace($jsontext, '', -1); // to get rid of extra comma
 $jsontext .= "}]";
 print_r($jsontext);
 */
+$peticiones = array($p1,$p2);
+$response = array("peticiones" => $peticiones);
 header('Content-Type: application/json');
-echo json_encode($p);
+echo json_encode($response);
 ?>
