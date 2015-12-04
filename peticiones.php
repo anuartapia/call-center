ea256a5c04e95cfae160e0a4706838c40024b46a
Anuar Tapia <anuartap@ciencias.unam.mx>
2015-12-04 12:22:35 -0600

    Crea archivo peticiones.php

---
+++ peticiones.php
@@ -0,0 +1,19 @@
+<?php
+$p = array(
+			"cliente" => "Jessi Jaime",
+      "telefono" => 0123456789,
+      "fecha" => date('ymd'),
+      "imagenMapa" => "https://cbtelevision.com.mx/wp-content/uploads/2015/06/619.jpg",
+      "calleOrigen" => "16 de Septiembre",
+      "numeroOrigen" => "15A",
+      "coloniaOrigen" => "Tepepan",
+      "delegacionOrigen" => "Xochimilco",
+      "calleDestino" => "Grimmauld Place",
+      "numeroDestino" => "12",
+      "coloniaDestino" => "London",
+      "delegacionDestino" => "UK",
+      "cancelada": false,
+      "atendida": false
+      );
+print_r($p);
+?>
